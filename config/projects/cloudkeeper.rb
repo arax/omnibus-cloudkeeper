9fb6d078f171ce6a243afff5531f55de149b51e5
Boris Parak <parak@cesnet.cz>
2017-03-25 15:17:04 +0100

    Initial commit
    Skeleton omnibus builder

---
+++ config/projects/cloudkeeper.rb
@@ -0,0 +1,41 @@
+require 'facter'
+
+name 'cloudkeeper'
+maintainer 'Boris Parak <parak@cesnet.cz>'
+homepage 'https://github.com/the-cloudkeeper-project/cloudkeeper'
+description 'Synchronize cloud appliances between AppDB and cloud platforms.'
+
+install_dir     '/opt/cloudkeeper'
+build_version   "1.0.0"
+build_iteration 1
+
+override :rubygems, :version => '2.6.8'
+## WARN: do not forget to change RUBY_VERSION in the postinst script
+##       when switching to a new minor version
+override :ruby, :version => '2.2.6'
+
+# creates required build directories
+dependency 'preparation'
+
+# cloudkeeper dependencies/components
+dependency 'cloudkeeper'
+
+# version manifest file
+dependency 'version-manifest'
+
+# tweaking package-specific options
+package :deb do
+  vendor 'CESNET, Grid Department <cloud@metacentrum.cz>'
+  license 'Apache License, Version 2.0'
+  priority 'extra'
+  section 'net'
+end
+
+package :rpm do
+  vendor 'CESNET, Grid Department <cloud@metacentrum.cz>'
+  license 'Apache License, Version 2.0'
+  category 'Applications/System'
+end
+
+exclude '\.git*'
+exclude 'bundler\/git'
---
+++ config/software/cloudkeeper.rb
@@ -0,0 +1,22 @@
+name "cloudkeeper"
+## WARN: do not forget to change CLOUDKEEPER_VERSION in the postinst script
+default_version "1.0.0"
+
+dependency "ruby"
+dependency "rubygems"
+dependency "liblzma"
+
+env = {
+  "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+  "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+  "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
+}
+
+build do
+  gem "install cloudkeeper -n #{install_dir}/bin --no-rdoc --no-ri -v #{version}", :env => env
+  delete "#{install_dir}/embedded/docs"
+  delete "#{install_dir}/embedded/share/man"
+  delete "#{install_dir}/embedded/share/doc"
+  delete "#{install_dir}/embedded/ssl/man"
+  delete "#{install_dir}/embedded/info"
+end
