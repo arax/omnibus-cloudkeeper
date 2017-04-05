b82dd72644f5472fa864155b2ee66dc2c82dd00f
Boris Parak <parak@cesnet.cz>
2017-04-05 11:46:31 +0200

    Updating to nginx v1.10.x with custom err log path

---
+++ config/software/nginx.rb
@@ -0,0 +1,47 @@
+#
+# Copyright 2012-2016 Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+name "nginx"
+default_version "1.10.2"
+
+dependency "pcre"
+dependency "openssl"
+
+source url: "http://nginx.org/download/nginx-#{version}.tar.gz"
+
+version("1.10.2") { source md5: "e8f5f4beed041e63eb97f9f4f55f3085" }
+
+relative_path "nginx-#{version}"
+
+build do
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  command "./configure" \
+          " --prefix=#{install_dir}/embedded" \
+          " --error-log-path=stderr" \
+          " --with-http_ssl_module" \
+          " --with-http_stub_status_module" \
+          " --with-ipv6" \
+          " --with-debug" \
+          " --with-cc-opt=\"-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include\"" \
+          " --with-ld-opt=-L#{install_dir}/embedded/lib", env: env
+
+  make "-j #{workers}", env: env
+  make "install", env: env
+
+  # Ensure the logs directory is available on rebuild from git cache
+  touch "#{install_dir}/embedded/logs/.gitkeep"
+end
