0775fabe268dc96794e2744031c89b6a9be12279
Boris Parak <parak@cesnet.cz>
2018-01-04 14:08:07 +0100

    Fixed systemd unit, bumped build interation

--- config/projects/cloudkeeper.rb
+++ config/projects/cloudkeeper.rb
@@ -5,7 +5,7 @@ description 'Synchronize cloud appliances between AppDB and cloud platforms.'
 
 install_dir     '/opt/cloudkeeper'
 build_version   "1.6.0"
-build_iteration 2
+build_iteration 3
 
 override :rubygems, :version => '2.6.8'
 ## WARN: do not forget to change RUBY_VERSION in the postinst script
